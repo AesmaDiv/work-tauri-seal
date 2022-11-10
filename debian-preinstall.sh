22855bcd74475665071896706c8d0b8f6a397ab4
AesmaDiv <jhwh.0001@gmail.com>
2022-11-10 17:24:16 +0500

    динамическая БД и универсальный провайдер точек

---
+++ debian-preinstall.sh
@@ -0,0 +1,9 @@
+#/usr/bin/sh
+sudo apt install libwebkit2gtk-4.0-dev \
+    build-essential \
+    curl \
+    wget \
+    libssl-dev \
+    libgtk-3-dev \
+    libayatana-appindicator3-dev \
+    librsvg2-dev
